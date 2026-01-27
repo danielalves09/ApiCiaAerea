3f7a7d44f951fdecd9d20e150cbf3c813ac67f1d
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2026-01-27 17:43:10 -0300

    Mapeando a entidade Piloto

---
+++ EntityConfigurations/PilotoConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using CiaAerea.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CiaAerea.EntityConfigurations
+{
+    public class PilotoConfiguration : IEntityTypeConfiguration<Piloto>
+    {
+        public void Configure(EntityTypeBuilder<Piloto> builder)
+        {
+            builder.ToTable("Pilotos");
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Matricula)
+               .IsRequired()
+               .HasMaxLength(10);
+
+            builder.HasIndex(p => p.Matricula)
+                .IsUnique();
+
+
+
+
+        }
+    }
+}
