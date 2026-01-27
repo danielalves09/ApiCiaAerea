39ae3f160c3d8baea443b7fc648fe54c831a5a22
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2026-01-27 17:55:33 -0300

    Criando Controller de Aeronaves

---
+++ Controllers/AeronaveController.cs
@@ -0,0 +1,24 @@
+﻿using CiaAerea.Services;
+using CiaAerea.ViewModels.Aeronave;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CiaAerea.Controllers
+{
+    [Route("api/aeronaves")]
+    public class AeronaveController : ControllerBase
+    {
+        private readonly AeronaveService _aeronaveService;
+
+        public AeronaveController(AeronaveService aeronaveService)
+        {
+            _aeronaveService = aeronaveService;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarAeronave([FromBody] AdicionarAeronaveViewModel model)
+        {
+            var aeronave = _aeronaveService.AdicionarAeronave(model);
+            return Ok(aeronave);
+        }
+    }
+}
