2e250816d5bf3b767534de91e1fc9a2d2bbb7fa8
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2026-01-27 17:34:50 -0300

    Entidade canelamento e voo

---
+++ Entities/Cancelamento.cs
@@ -0,0 +1,20 @@
+﻿namespace CiaAerea.Entities
+{
+    public class Cancelamento
+    {
+        public int Id { get; set; }
+        public string Motivo { get; set; }
+        public DateTime DataHoraNotificacao { get; set; }
+
+        public int VooId { get; set; }
+        public Voo Voo { get; set; } = null!;
+
+
+        public Cancelamento(string motivo, DateTime dataHoraNotificacao, int vooId)
+        {
+            Motivo = motivo;
+            DataHoraNotificacao = dataHoraNotificacao;
+            VooId = vooId;
+        }
+    }
+}
