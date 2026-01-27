2d34451ff428cad6b8b690720642383c2a9e772b
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2026-01-27 17:33:55 -0300

    Entidade Piloto

---
+++ Entities/Piloto.cs
@@ -0,0 +1,17 @@
+﻿namespace CiaAerea.Entities
+{
+    public class Piloto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public ICollection<Voo> Voos { get; set; } = null!;
+
+        public Piloto(string nome, string matricula)
+        {
+            Nome = nome;
+            Matricula = matricula;
+        }
+
+    }
+}
